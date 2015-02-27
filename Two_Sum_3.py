7889008e22bf6f27dbe673b73fd1d324e37ff23c
Lei <jay6413682@gmail.com>
2015-02-26 18:48:18 -0800

    add problem two sum 3

---
+++ Two_Sum_3.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, sys
+
+'''
+Question: Design and implement a TwoSum class. It should support the following operations: add and find. add(input)  – Add the number input to an internal data structure. find(value) – Find if there exists any
+pair of numbers which sum is equal to the value. For example, add(1); add(3); add(5); find(4)true; find(7)false
+'''
+class TwoSum1:
+    def __init__(self,lst_sum,lst):
+        self.lst_sum=lst_sum
+        self.lst=lst
+        
+    def add(self,x):
+        if len(self.lst)==0:
+            self.lst.append(x)
+            self.lst_sum.append(x)
+        
+        else:
+            for element in self.lst:
+                sm=x+element
+                if sm not in self.lst_sum:
+                    self.lst_sum.append(sm)
+            self.lst.append(x)
+        return 
+        
+    def find(self,s):
+        return True if s in self.lst_sum else False
+
+
+class TwoSum:
+    def __init__(self,dct):
+        self.dct=dct
+        
+    def add(self,x):
+        count = self.dct[x] if self.dct.has_key(x) else 0
+        self.dct.update({x:count+1})
+        return 
+        
+    def find(self,s):
+        for key,value in self.dct.items():
+            target = s - key
+            if target == key:
+                if value >= 2:
+                    return True
+            else: 
+                if target in self.dct.keys():
+                    return True         
+        return False   
+
+
+
+
