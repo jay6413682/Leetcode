e16078776ac352db580d123aba71bfae67a68f22
Lei <jay6413682@gmail.com>
2015-03-19 16:39:27 -0700

    develop question 3 and unit test cases

--- Two_Sum_3.py
+++ Two_Sum_3.py
@@ -6,6 +6,7 @@ import os, sys
 Question: Design and implement a TwoSum class. It should support the following operations: add and find. add(input)  – Add the number input to an internal data structure. find(value) – Find if there exists any
 pair of numbers which sum is equal to the value. For example, add(1); add(3); add(5); find(4)true; find(7)false
 '''
+from copy import deepcopy
 class TwoSum1:
     def __init__(self,lst_sum,lst):
         self.lst_sum=lst_sum
@@ -26,6 +27,51 @@ class TwoSum1:
         
     def find(self,s):
         return True if s in self.lst_sum else False
+
+class TwoSum2:
+    def __init__(self,lst):
+        self.lst=lst
+        
+    def add(self,x):
+        self.lst=sorted(self.lst)
+        L=0
+        if len(self.lst)==0:
+            self.lst.append(x)
+            return
+        else: 
+            R=len(self.lst)-1
+            M=(L+R)/2
+            while L<=R:
+                if x>self.lst[R]:
+                    self.lst.append(x)
+                    return
+                elif x<self.lst[L]:
+                    self.lst.insert(M,x)
+                    return
+                elif x == self.lst[M] or L==R:
+                    self.lst.insert(M+1,x)
+                    return 
+                elif x>self.lst[M]:
+                    L=deepcopy(M)+1
+                    M=(L+R)/2
+                elif x<self.lst[M]:
+                    R=deepcopy(M)
+                    M=(L+R)/2
+                
+        return 
+        
+    def find(self,s):
+        i=0
+        j=len(self.lst)-1
+        while i < j:
+            if self.lst[i]+self.lst[j]==s:
+                return True
+            elif self.lst[i]+self.lst[j]>s:
+                j-=1
+            else:
+                i+=1
+        return False
+        
 
 
 class TwoSum:
--- test_two_sum_3.py
+++ test_two_sum_3.py
@@ -7,7 +7,7 @@ import pytest
 import sys
 from Two_Sum_3 import *
 #pytest fixture example
-@pytest.fixture(scope="function",params=[0,1])
+@pytest.fixture(scope="function",params=[2])
 def config_module(request):
     try:
         print ("setup_module      module:%s" % __name__)
@@ -15,6 +15,8 @@ def config_module(request):
             obj=TwoSum({})
         elif request.param == 1:
             obj=TwoSum1([],[])
+        elif request.param == 2:
+            obj=TwoSum2([])
         def fin():
             print "teardown_module      module:%s" % __name__
             #del obj
@@ -27,7 +29,7 @@ def config_module(request):
         sys.exit(1)
 
 
-@pytest.mark.parametrize("nums,target,expectation", [([-1,1,0],2,False),([1,2,3,4,5],9,True),([1,1,1],2,True),([0.1,0.9],1,True)])        
+@pytest.mark.parametrize("nums,target,expectation", [([-1,1,0],2,False),([100,10,0],110,True),([1,2,3,4,5],9,True),([1,1,1],2,True),([0.1,0.9],1,True)])        
 def test_two_sum(config_module,nums,target,expectation):
     for element in nums:
         config_module.add(element)
