cef9ba941f0fedd6ccd4550a81b6c3cb4befac6e
Lei <jay6413682@gmail.com>
2015-06-09 22:59:45 -0700

    Add question 6 and its unit test cases

---
+++ reverse_str_word_by_word_6.py
@@ -0,0 +1,82 @@
+'''
+Created on May 3, 2015
+
+@author: ljiang
+Given an input string s, reverse the string word by word.
+For example, given s = "the sky is blue", return "blue is sky the".
+'''
+class reverse_str_word_by_word_6:
+    def __init__(self,words):
+        self.words=words
+        
+    def reverse1(self):
+        ##@my name is&Lei;my name\tis Lei;my name\nis Lei
+        word=""
+        word_lst=[]
+        reversed_words=""
+        for c in self.words:
+            if not (c.isalpha() or c==" "):
+                return False
+        
+        else:
+            i=0
+            for ch in self.words:
+                if ch==" ":
+                    word_lst.append(word)
+                    word=""
+
+                elif ch.isalpha():
+                    word=word+ch                
+                if i==len(self.words)-1:
+                    word_lst.append(word)
+                    word=""
+                i+=1
+            
+            for wd in word_lst:
+                if wd=="":
+                    continue
+                else:
+                    reversed_words=wd+" "+reversed_words
+            if reversed_words[len(reversed_words)-1]==" ":
+                reversed_words=reversed_words.strip(' ')
+        return reversed_words                    
+
+    def reverse2(self):
+        for c in self.words:
+            if not (c.isalpha() or c==" "):
+                return False
+        words_lst=self.words.strip(' \t\n').split()
+        reversed_words=""
+        for word in words_lst:
+            if words_lst.index(word)==0:
+                reversed_words=word
+            else:
+                reversed_words=word+" "+reversed_words
+                
+        return reversed_words
+                
+    def reverse3(self):
+        i=len(self.words)-1
+        start=len(self.words)-1
+        end=len(self.words)-1
+        words_list=[]
+        reversed_words=""
+        while i >=0:
+            if self.words[i]==" ":
+                if start!=end:
+                    words_list.append(self.words[start+1:end+1])
+                start-=1
+                end=start
+            else:
+                start-=1
+            i-=1
+        j=0
+        for word in words_list:
+            if j==0:
+                reversed_words=word
+            else:
+                reversed_words=reversed_words+" "+word
+            j+=1
+        return reversed_words
+        
+        
