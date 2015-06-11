faacebf81b889379376b9ab2e73feb29e57ae274
Lei <jay6413682@gmail.com>
2015-06-11 15:02:03 -0700

    Add question 9

---
+++ valid_number_9.py
@@ -0,0 +1,23 @@
+'''
+Created on Jun 11, 2015
+
+@author: ljiang
+Validate if a given string is numeric.
+'''
+class valid_number_9:
+    def __init__(self):
+        pass
+    def valid_num(self,num):
+        num=num.strip(" ")
+        #sign=0
+        if num=="":
+            return False
+        elif num=='.':
+            return False
+        elif num[0]=='+' or num[0]=='-':
+            num=num.lstrip(num[0])
+        for i in num:
+            if not (i.isdigit() or i=='.'):
+                return False            
+        return True
+                
